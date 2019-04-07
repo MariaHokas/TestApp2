a6ad37ed698f3a1543096812e7333b2089989747
Ahokas Maria Katariina <maria.ahokas@stud.pointcollege.fi>
2019-04-07 11:52:35 +0300

    Chart added

--- TestApp2/Controllers/ReportsController.cs
+++ TestApp2/Controllers/ReportsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestApp2.Models;
+using TestApp2.ViewModels;
 
 namespace TestApp2.Controllers
 {
@@ -12,6 +14,44 @@ namespace TestApp2.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetTimesheetCounts(string onlyComplete)
+        {
+            ReportChartDataViewModel model = new ReportChartDataViewModel();
+
+            TimesheetEntities entities = new TimesheetEntities();
+            try
+            {
+                model.Labels = (from wa in entities.WorkAssignments
+                                orderby wa.Id_WorkAssignment
+                                select wa.Title).ToArray();
+
+                if (onlyComplete == "1")
+                {
+                    model.Counts = (from ts in entities.Timesheets
+                                    where (ts.WorkCompleted == true)
+                                    orderby ts.id_WorkAssignment
+                                    group ts by ts.id_WorkAssignment into grp
+                                    select grp.Count()).ToArray();
+                }
+                else
+                {
+                    model.Counts = (from ts in entities.Timesheets
+                                    orderby ts.id_WorkAssignment
+                                    group ts by ts.id_WorkAssignment into grp
+                                    select grp.Count()).ToArray();
+
+
+                }
+
+            }
+
+            finally
+            {
+                entities.Dispose();
+            }
+            return Json(model, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
